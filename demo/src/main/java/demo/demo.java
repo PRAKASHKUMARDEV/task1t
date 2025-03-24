6a8158267d197a76b14a0dba293eec72ed1bf7f8
PRAKASHKUMAR <105591571+PRAKASHKUMARDEV@users.noreply.github.com>
2025-03-24 14:02:13 +0530

    Update demo.java

--- demo/src/main/java/demo/demo.java
+++ demo/src/main/java/demo/demo.java
@@ -5,7 +5,8 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("this is firsi git ");
-    System.out.println("this is chane for  push");
+    System.out.println("this is chane for   modified by qa2 push");
+		
 	}
 	
 
